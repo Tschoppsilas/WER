d202d8cd79e81f17fb1d3559f5bb0f9839072168
Tschoppsilas <silas.tschopp@breitband.ch>
2024-12-04 16:28:50 +0100

    LE1

--- Bayes.py
+++ Bayes.py
@@ -2,8 +2,7 @@
 def bayes_theorem(n):
     P_A = float(input(
         "Die Wahrscheinlichkeit in %, dass das Ereignis A (P(A)) wahr ist (z. B. gezinkter Würfel)")) / 100  # Wahrscheinlichkeit, dass das Ereignis A wahr ist (z. B. gezinkter Würfel)
-    P_neg_A = float(input(
-        " Die Wahrscheinlichkeit in %, dass das Ereignis A (P(NA))nicht wahr ist (z. B. fairer Würfel)")) / 100  # Wahrscheinlichkeit, dass das Ereignis A nicht wahr ist (z. B. fairer Würfel)
+    P_neg_A = 1- P_A  # Wahrscheinlichkeit, dass das Ereignis A nicht wahr ist (z. B. fairer Würfel)
     P_B_given_A = float(input(
         "Die Wahrscheinlichkeit in %, dass B eintritt, wenn A wahr ist (P(B|A))(z. B. Wahrscheinlichkeit für eine 5 bei gezinktem Würfel)")) / 100  # Wahrscheinlichkeit, dass B eintritt, wenn A wahr ist (z. B. Wahrscheinlichkeit für eine 5 bei gezinktem Würfel)
     P_B_given_neg_A = float(input(
