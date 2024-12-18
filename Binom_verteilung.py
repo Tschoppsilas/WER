c0838e6fb36c88d1a308359234104c3ab1a3a409
Tschoppsilas <silas.tschopp@breitband.ch>
2024-12-18 11:26:13 +0100

    LE2 Diskrete Variablen

--- Binom_verteilung.py
+++ Binom_verteilung.py
@@ -5,4 +5,8 @@ def Binominialverteilung():
     k = int(input("k (Die Anzahl der Erfolge / im Poker die grösser der Hand (bsp 5 Karten = 1 Hand)"))
     p = float(input("Wahrscheinlichkeit in % für erfolg"))/100
     resultat = math.comb(n, k) * (p**k) * ((1-p)**(n-k)) *100
-    print(f"f{resultat}%")
+    print(f"{resultat}%")
+    erwartungswert = n*p
+    print(f"Erwartungswert E(X): {erwartungswert}")
+    varianz = erwartungswert *(1-p)
+    print(f"Varianz: {varianz}")
---
+++ Erwartungswert.py
@@ -0,0 +1,6 @@
+import math
+
+def Erwartungswert():
+    Frage1 = int(input("Handelt es sich um Diskrete Variablen? (Merkmale) Ja = j, Nein = n"))
+    if Frage1 == "j":
+        int(input(""))
